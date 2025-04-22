910762af6f773c16aa48e3860bf3dba35f4659fc
Edmond <edmondp98@mailbox.unideb.hu>
2025-04-22 09:37:51 +0200

    Message

--- src/main/java/com/esen/bookstore/model/Bookstore.java
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -1,9 +1,12 @@
 package com.esen.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Map;
 
+@Data
 @Entity
 public class Bookstore {
 
@@ -17,56 +20,6 @@ public class Bookstore {
     private Double moneyInCashRegister;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
     private Map<Book, Integer> inventory;
-
-    public Bookstore() {
-    }
-
-    public Bookstore(Long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
-        this.id = id;
-        this.location = location;
-        this.priceModifier = priceModifier;
-        this.moneyInCashRegister = moneyInCashRegister;
-        this.inventory = inventory;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Double getPriceModifier() {
-        return priceModifier;
-    }
-
-    public void setPriceModifier(Double priceModifier) {
-        this.priceModifier = priceModifier;
-    }
-
-    public Double getMoneyInCashRegister() {
-        return moneyInCashRegister;
-    }
-
-    public void setMoneyInCashRegister(Double moneyInCashRegister) {
-        this.moneyInCashRegister = moneyInCashRegister;
-    }
-
-    public Map<Book, Integer> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Map<Book, Integer> inventory) {
-        this.inventory = inventory;
-    }
 }
